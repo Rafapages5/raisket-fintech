24252906b1b25d1e3274204ac8ac92607ba5e566
rafael <wolsburg20@gmail.com>
2025-11-23 15:48:48 -0600

    Fix: Corregir typo en next.config.ts (ignoreBuildErrors)

--- next.config.ts
+++ next.config.ts
@@ -42,50 +42,6 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.bbva.mx',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.santander.com.mx',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.banorte.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.citibanamex.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.hsbc.com.mx',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.scotiabank.com.mx',
-      },
-      {
-        protocol: 'https',
         hostname: '**.supabase.co',
       },
       {
